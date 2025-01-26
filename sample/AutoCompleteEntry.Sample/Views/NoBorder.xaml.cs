0a09c4d097ce6c97518662c9591a854dfbb6013c
Giulio Zanchetta <zank@outlook.it>
2025-01-26 22:05:47 +0100

    Add No Border tab and related view
    - Introduced a new tab titled "No Border" in the app shell.
    - Created a new XAML page for the No Border feature with bindings.
    - Added code-behind to handle user interactions and data binding for the AutoCompleteEntry control.
    - Updated project file to include references for the new view.

---
+++ sample/AutoCompleteEntry.Sample/Views/NoBorder.xaml.cs
@@ -0,0 +1,47 @@
+using AutoCompleteEntry.Sample.ViewModels;
+
+namespace AutoCompleteEntry.Sample.Views
+{
+    public partial class NoBorder : ContentPage
+    {
+        private SampleViewModel ViewModel => BindingContext as SampleViewModel;
+
+        public NoBorder()
+        {
+            BindingContext = new SampleViewModel();
+
+            InitializeComponent();
+        }
+
+        private void AutoCompleteEntry_TextChanged(object sender, zoft.MauiExtensions.Controls.AutoCompleteEntryTextChangedEventArgs e)
+        {
+            // Only get results when it was a user typing, 
+            // otherwise assume the value got filled in by TextMemberPath 
+            // or the handler for SuggestionChosen.
+            if (e.Reason == zoft.MauiExtensions.Controls.AutoCompleteEntryTextChangeReason.UserInput)
+            {
+                //Set the ItemsSource to be your filtered dataset
+                ViewModel.FilterList((sender as zoft.MauiExtensions.Controls.AutoCompleteEntry).Text);
+            }
+        }
+
+        private void AutoCompleteEntry_SuggestionChosen(object sender, zoft.MauiExtensions.Controls.AutoCompleteEntrySuggestionChosenEventArgs e)
+        {
+            // Set sender.Text. You can use args.SelectedItem to build your text string.
+            ViewModel.SelectedItem = e.SelectedItem as ListItem;
+        }
+
+        private void AutoCompleteEntry_CursorPositionChanged(object sender, zoft.MauiExtensions.Controls.AutoCompleteEntryCursorPositionChangedEventArgs e)
+        {
+            ViewModel.CursorPosition = e.CursorPosition;
+        }
+
+        private void AutoCompleteEntry_Completed(object sender, EventArgs e)
+        {
+            if (sender is zoft.MauiExtensions.Controls.AutoCompleteEntry autoCompleteEntry)
+            {
+                ViewModel.SelectedItem = ViewModel.GetExactMatch(autoCompleteEntry.Text);
+            }
+        }
+    }
+}
